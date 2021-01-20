ef2e3182c1a7a4f3adbae919d34d596349cc546f
Robinson <robinpaulista85@gmail.com>
2021-01-20 13:29:13 -0300

    Challenge one

---
+++ desafio1.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Digite um número inteiro: "))
+numero2 = int(input("Digite um segundo número inteiro: "))
+numero3 = int(input("Digite um terceiro número inteiro: "))
+numero4 = int(input("Digite um quarto número inteiro: "))
+
+soma = numero1 + numero2 + numero3 + numero4 
+
+print("A soma dos números será: ", soma)
